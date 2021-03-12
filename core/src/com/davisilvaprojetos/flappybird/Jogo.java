21317b315ec8eb7f527bac5a041b5ebf90bcaa04
Davi Francisco da Silva <43782019+davif10@users.noreply.github.com>
2021-03-12 19:54:22 -0300

    -Movimentando o pássaro -Animando o pássaro

--- core/src/com/davisilvaprojetos/flappybird/Jogo.java
+++ core/src/com/davisilvaprojetos/flappybird/Jogo.java
@@ -9,30 +9,50 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import java.util.Random;
 
 public class Jogo extends ApplicationAdapter {
-	private SpriteBatch batch;
-	private Texture passaro;
-	int contador = 0;
+    private SpriteBatch batch;
+    private Texture[] passaros;
+    private Texture fundo;
+    private int movimentoX = 0;
+    private int movimentoY = 0;
 
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		passaro = new Texture("passaro1.png");
-	}
+    //Atributos de configuracoes
+    private float larguraDispositivo;
+    private float alturaDispositivo;
+    private float variacao = 0;
 
-	@Override
-	public void render () {
-		batch.begin();
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        passaros = new Texture[3];
+        passaros[0] = new Texture("passaro1.png");
+        passaros[1] = new Texture("passaro2.png");
+        passaros[2] = new Texture("passaro3.png");
 
+        fundo = new Texture("fundo.png");
+        larguraDispositivo = Gdx.graphics.getWidth();
+        alturaDispositivo = Gdx.graphics.getHeight();
+    }
 
-		batch.draw(passaro, contador, 0);
+    @Override
+    public void render() {
+        batch.begin();
 
-		contador+=2;
+        if (variacao > 3)
+            variacao = 0;
 
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
+        batch.draw(fundo, 0, 0, larguraDispositivo, alturaDispositivo);
+        batch.draw(passaros[ (int) variacao], movimentoX, alturaDispositivo/2);
 
-	}
+        variacao+= Gdx.graphics.getDeltaTime() * 10;
+
+        movimentoX++;
+        movimentoY++;
+
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+
+    }
 }
